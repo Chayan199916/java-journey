603e796e1dbda5daba3f84191a9a59462e026542
Chayan Kumar Sengupta <swapnomoy199916@gmail.com>
2021-07-18 17:29:27 +0530

    added comments; cookie management; servlet context, config and annotation config

--- Servlet-JSP/jsp-servlet/DemoApplication/src/main/java/com/chayan/AddServlet.java
+++ Servlet-JSP/jsp-servlet/DemoApplication/src/main/java/com/chayan/AddServlet.java
@@ -1,16 +1,26 @@
 package com.chayan;
 
 import java.io.IOException;
-//import java.io.PrintWriter;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+@WebServlet("/add")
 public class AddServlet extends HttpServlet
 {
+	/*
+	 *	service method calls doGet by default if not mentioned.
+	 *	getWriter returns object of PrintWriter
+	 */
 //	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 //		int first = Integer.parseInt(req.getParameter("num1"));
 //		int second = Integer.parseInt(req.getParameter("num2"));
@@ -22,13 +32,56 @@ public class AddServlet extends HttpServlet
 		int first = Integer.parseInt(req.getParameter("num1"));
 		int second = Integer.parseInt(req.getParameter("num2"));
 		int result = first + second;
-//		PrintWriter out = res.getWriter();
+		
+		PrintWriter out = res.getWriter();
 //		out.println(first + second);
 		
+		/*
+		 * Using ServletContext
+		 * */
+//		ServletContext context = getServletContext();
+//		String name = context.getInitParameter("name");
+		
+		/*
+		 * Using ServletConfig
+		 * */
+//		ServletConfig cfg = getServletConfig();
+//		String name = cfg.getInitParameter("name");
+//		
+//		out.println(name);
+
+		/*
+		 * sending data from one servlet to other servlet using req object
+		 * */
+		
 //		req.setAttribute("result", result);
+		
+		/*
+		 * dispatching another servlet which has been mapped to /sq url pattern from this servlet
+		 * */
 //		RequestDispatcher requestDispatcher = req.getRequestDispatcher("sq");
 //		requestDispatcher.forward(req, res);
 		
-		res.sendRedirect("sq?result=" + result); // URL rewriting
+		/*
+		 * another way of sending req from one servlet to another one. url rewriting being used to send data 
+		 */
+//		res.sendRedirect("sq?result=" + result); // URL rewriting
+		
+		/*session management*/
+//		HttpSession session = req.getSession();		
+//		session.setAttribute("result", result);
+		
+		/*Cookie management
+		 * this cookie will be sent to client with response
+		 * and then when client will send req to url "/sq", inside 
+		 * the servlet we will accept cookie and the data stored in it.
+		 * */
+//		Cookie cookie = new Cookie("result", result + "");
+//		res.addCookie(cookie);
+		
+		
+		
+//		res.sendRedirect("sq");
+		
 	}
 }
--- Servlet-JSP/jsp-servlet/DemoApplication/src/main/java/com/chayan/SquareServlet.java
+++ Servlet-JSP/jsp-servlet/DemoApplication/src/main/java/com/chayan/SquareServlet.java
@@ -3,14 +3,44 @@ package com.chayan;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+@WebServlet("/sq")
 public class SquareServlet extends  HttpServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		int k = Integer.parseInt(req.getParameter("result"));
-		PrintWriter out = res.getWriter();
-		out.println(k * k);
+		HttpSession session = req.getSession();
+		/*
+		 * to remove value from session : session.removeAttribute();
+		 * */
+		/*taking data from session*/
+//		int k = (int) session.getAttribute("result");
+		
+		
+//		int k = Integer.parseInt(req.getParameter("result"));
+		
+		/*taking data from cookie*/
+//		Cookie cookies[] = req.getCookies();
+		/*
+		 * So now we have all the cookies, we have to 
+		 * check the cookie name to get the cookie we want
+		 * */
+//		int k = 0;
+//		for(Cookie cookie : cookies) {
+//			if(cookie.getName().equals("result")) {
+//				k = Integer.parseInt(cookie.getValue());
+//			}
+//		}
+//		
+//		PrintWriter out = res.getWriter();
+//		out.println(k * k);
+		
+		
+		
+		
 	}
 }
